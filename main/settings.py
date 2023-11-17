bfbefe8d5c9dd219da9ad963e2b2bb674b30b640
Anirudh MK <its.me.anirudh.mk@gmail.com>
2023-11-17 12:05:36 +0530

    database changed to mysl and env added

--- main/settings.py
+++ main/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config as decouple_config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -73,14 +75,23 @@ WSGI_APPLICATION = 'main.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": decouple_config('DATABASE_ENGINE'),
+        "NAME": decouple_config('DATABASE_NAME'),
+        "USER": decouple_config('DATABASE_USER'),
+        "PASSWORD": decouple_config('DATABASE_PASSWORD'),
+        "HOST": decouple_config('DATABASE_HOST'),
+        "PORT": decouple_config('DATABASE_PORT'),
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
